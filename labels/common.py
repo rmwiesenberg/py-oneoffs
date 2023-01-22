53c52cc779d4ab7b6e827fb46666b2a7666aaddf
Ryan Wiesenberg <rmwiesenberg@gmail.com>
2023-01-22 16:12:27 -0500

    add international label making

--- labels/common.py
+++ labels/common.py
@@ -1,6 +1,5 @@
 from abc import ABC
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Optional
 
 
@@ -13,8 +12,19 @@ class Address(ABC):
     address2: Optional[str]
 
     city: str
+    zipcode: str
+
+    def __str__(self) -> str:
+        ...
+
+    @classmethod
+    def from_str(cls, address_str: str) -> 'Address':
+        ...
+
+
+@dataclass
+class AddressUS(Address):
     state: str
-    zipcode: int
 
     def __str__(self) -> str:
         address_str = f'{self.address1}\n'
@@ -24,40 +34,7 @@ class Address(ABC):
         return address_str
 
     @classmethod
-    def from_str(cls, address_str: str) -> 'Address':
-        ...
-
-
-@dataclass
-class AddressInternational(Address):
-    country: str
-
-    def __str__(self) -> str:
-        return super().__str__() + f' {self.country}'
-
-    @classmethod
-    def from_str(cls, address_str: str) -> 'AddressInternational':
-        address1 = ''
-        address2 = None
-        city = ''
-        state = ''
-        zipcode = 0
-        country = ''
-
-        return AddressInternational(
-            address1=address1,
-            address2=None,
-            city=city,
-            state=state,
-            zipcode=zipcode,
-            country=country
-        )
-
-
-@dataclass
-class AddressUS(Address):
-    @classmethod
-    def from_str(cls, address_str: str) -> '':
+    def from_str(cls, address_str: str) -> 'AddressUS':
         address_split = address_str.split(',')
 
         address1 = address_split[0]
@@ -73,7 +50,7 @@ class AddressUS(Address):
 
         locality_split = locality.strip().split(' ')
         state = locality_split[0]
-        zipcode = int(locality_split[-1])
+        zipcode = locality_split[-1]
 
         return AddressUS(
             address1=address1.strip(),
@@ -84,9 +61,45 @@ class AddressUS(Address):
         )
 
 
+@dataclass
+class AddressInternational(Address):
+    country: str
+
+    def __str__(self) -> str:
+        address_str = f'{self.address1}\n'
+        if self.address2:
+            address_str += f'{self.address2}\n'
+        address_str += f'{self.city}, {self.country} {self.zipcode}'
+        return address_str
+
+    @classmethod
+    def from_str(cls, address_str: str) -> 'AddressInternational':
+        address_split = address_str.split(',')
+
+        address1 = address_split[0]
+        city = address_split[-3]
+        zipcode = address_split[-2]
+        country = address_split[-1]
+
+        address2 = None
+        if len(address_split) == 5:
+            address2 = address_split[1]
+        elif len(address_split) != 4:
+            quick_str = '\n'.join(address_split)
+            raise ValueError(f'Weird address length...\n{quick_str}')
+
+        return AddressInternational(
+            address1=address1,
+            address2=address2,
+            city=city,
+            zipcode=zipcode,
+            country=country
+        )
+
+
 def address_from_str(address_str: str) -> Address:
     try:
-        int(address_str.split(' ')[-1])
+        int(address_str.split(' ')[-1].split('-')[0])
     except ValueError:
         return AddressInternational.from_str(address_str)
     else:
