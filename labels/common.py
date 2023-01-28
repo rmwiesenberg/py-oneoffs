9bcccbf5570604328011ec0b759dea4292484aea
Ryan Wiesenberg <rmwiesenberg@gmail.com>
2023-01-27 21:11:35 -0500

    allow infinitely long addresses

--- labels/common.py
+++ labels/common.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from dataclasses import dataclass
-from typing import Optional
+from typing import List, Optional
 
 
 STREET_TYPES = {'ln', 'st', 'ave', 'rd', 'avenue', 'dr', }
@@ -8,11 +8,15 @@ STREET_TYPES = {'ln', 'st', 'ave', 'rd', 'avenue', 'dr', }
 
 @dataclass
 class Address(ABC):
-    address1: str
-    address2: Optional[str]
+    addresses: List[str]
 
     city: str
     zipcode: str
+
+    def __post_init__(self):
+        self.addresses = [a.strip() for a in self.addresses]
+        self.city = self.city.strip()
+        self.zipcode = self.zipcode.strip()
 
     def __str__(self) -> str:
         ...
@@ -26,35 +30,33 @@ class Address(ABC):
 class AddressUS(Address):
     state: str
 
+    def __post_init__(self):
+        super().__post_init__()
+        self.state = self.state.strip()
+
     def __str__(self) -> str:
-        address_str = f'{self.address1}\n'
-        if self.address2:
-            address_str += f'{self.address2}\n'
-        address_str += f'{self.city}, {self.state} {self.zipcode}'
+        address_str = '\n'.join(self.addresses)
+        address_str += f'\n{self.city}, {self.state} {self.zipcode}'
         return address_str
 
     @classmethod
     def from_str(cls, address_str: str) -> 'AddressUS':
         address_split = address_str.split(',')
 
-        address1 = address_split[0]
-        city = address_split[-2]
-        locality = address_split[-1]
-
-        address2 = None
-        if len(address_split) == 4:
-            address2 = address_split[1]
-        elif len(address_split) != 3:
+        if len(address_split) < 3:
             quick_str = '\n'.join(address_split)
             raise ValueError(f'Weird address length...\n{quick_str}')
+
+        addresses = address_split[:-2]
+        city = address_split[-2]
+        locality = address_split[-1]
 
         locality_split = locality.strip().split(' ')
         state = locality_split[0]
         zipcode = locality_split[-1]
 
         return AddressUS(
-            address1=address1.strip(),
-            address2=address2.strip() if address2 else None,
+            addresses=addresses,
             city=city.strip(),
             state=state.strip(),
             zipcode=zipcode
@@ -65,32 +67,30 @@ class AddressUS(Address):
 class AddressInternational(Address):
     country: str
 
+    def __post_init__(self):
+        super().__post_init__()
+        self.country = self.country.strip()
+
     def __str__(self) -> str:
-        address_str = f'{self.address1}\n'
-        if self.address2:
-            address_str += f'{self.address2}\n'
-        address_str += f'{self.city}, {self.country} {self.zipcode}'
+        address_str = '\n'.join(self.addresses)
+        address_str += f'\n{self.city}, {self.country} {self.zipcode}'
         return address_str
 
     @classmethod
     def from_str(cls, address_str: str) -> 'AddressInternational':
         address_split = address_str.split(',')
 
-        address1 = address_split[0]
+        if len(address_split) < 4:
+            quick_str = '\n'.join(address_split)
+            raise ValueError(f'Weird address length...\n{quick_str}')
+
+        addresses = address_split[:-3]
         city = address_split[-3]
         zipcode = address_split[-2]
         country = address_split[-1]
 
-        address2 = None
-        if len(address_split) == 5:
-            address2 = address_split[1]
-        elif len(address_split) != 4:
-            quick_str = '\n'.join(address_split)
-            raise ValueError(f'Weird address length...\n{quick_str}')
-
         return AddressInternational(
-            address1=address1,
-            address2=address2,
+            addresses=addresses,
             city=city,
             zipcode=zipcode,
             country=country
@@ -110,6 +110,9 @@ def address_from_str(address_str: str) -> Address:
 class Person:
     name: str
     address: Address
+
+    def __post_init__(self):
+        self.name = self.name.strip()
 
     def __str__(self) -> str:
         return f'{self.name}\n{self.address}'
